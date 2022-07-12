f47c534be36766d1a82750c871a3a04eb19f5802
Bhopal Singh <bhopalsinghsis@gmail.com>
2022-07-12 16:01:59 -0400

    AWS CDK setup

--- README.md
+++ README.md
@@ -1,2 +1,39 @@
-# aws-sdk-lambda-stack-node
-AWS Lambda Stack Node JS
+# Write TypeScript Lambda functions in AWS CDK - Complete Guide
+
+## How to Use
+
+1. Clone the repository
+
+2. Install the dependencies
+
+```bash
+npm install
+```
+
+3. ONE TIME ONLY - if it is the First time, then bootstrap the repository (Only run this the very first time, SKIP after that)
+
+```bash
+npx aws-cdk bootstrap
+```
+
+5. CDk Synth
+
+```bash
+cdk synth
+```
+
+6. Create the CDK stack
+
+```bash
+cdk deploy
+```
+
+7. Open the AWS Console and the stack should be created in your default region
+
+8. Cleanup
+
+```bash
+cdk destroy
+```
+
+npx aws-cdk deploy --no-previous-parameters
---
+++ lib/cdk-starter-stack.ts
@@ -0,0 +1,22 @@
+import * as cdk from "aws-cdk-lib";
+import { NodejsFunction } from "aws-cdk-lib/aws-lambda-nodejs";
+import * as lambda from "aws-cdk-lib/aws-lambda";
+import * as path from "path";
+
+export class CdkStarterStack extends cdk.Stack {
+  constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    const myFunction = new NodejsFunction(this, "my-function", {
+      memorySize: 1024,
+      timeout: cdk.Duration.seconds(5),
+      runtime: lambda.Runtime.NODEJS_14_X,
+      handler: "main",
+      entry: path.join(__dirname, `/../src/my-lambda/index.ts`),
+      bundling: {
+        minify: false,
+        externalModules: ["aws-sdk"],
+      },
+    });
+  }
+}
