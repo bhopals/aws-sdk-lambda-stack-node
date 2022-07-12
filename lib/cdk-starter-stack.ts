38222aaaca59d26dbe58dffc4d11c5ab88444441
Bhopal Singh <bhopalsinghsis@gmail.com>
2022-07-12 18:56:08 -0400

    ci(npm): upgrade aws version

--- lib/cdk-starter-stack.ts
+++ lib/cdk-starter-stack.ts
@@ -8,10 +8,11 @@ export class CdkStarterStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const internalFunction = new NodejsFunction(this, "internal-lambda", {
+    /*** PRIVATE LAMBDA FUNCTION */
+    const internalLambda = new NodejsFunction(this, "internal-lambda", {
       memorySize: 1024,
       timeout: cdk.Duration.seconds(5),
-      runtime: lambda.Runtime.NODEJS_14_X,
+      runtime: lambda.Runtime.NODEJS_16_X,
       handler: "main",
       functionName: "internal-lambda",
       entry: path.join(__dirname, `/../src/lambda/internal.ts`),
@@ -21,11 +22,12 @@ export class CdkStarterStack extends cdk.Stack {
       },
     });
 
+    /*** PUBLIC LAMBDA FUNCTION */
     const publicLambda = new NodejsFunction(this, "public-lambda", {
       memorySize: 1024,
       functionName: "public-lambda",
       timeout: cdk.Duration.seconds(5),
-      runtime: lambda.Runtime.NODEJS_14_X,
+      runtime: lambda.Runtime.NODEJS_16_X,
       handler: "main",
       entry: path.join(__dirname, `/../src/lambda/index.ts`),
       bundling: {
@@ -34,21 +36,12 @@ export class CdkStarterStack extends cdk.Stack {
       },
     });
 
-    // publicLambda.
-    // const fnUrl = publicLambda.addFunctionUrl({
-    //   authType: lambda.FunctionUrlAuthType.NONE,
-    // });
+    const fnUrl = publicLambda.addFunctionUrl({
+      authType: lambda.FunctionUrlAuthType.NONE,
+    });
 
-    // new CfnOutput(this, "TheUrl", {
-    //   value: fnUrl.url,
-    // });
-
-    // const fnUrl = publicLambda.addFunctionUrl({
-    //   authType: lambda.FunctionUrlAuthType.NONE,
-    // });
-
-    // new CfnOutput(this, "TheUrl", {
-    //   value: fnUrl.url,
-    // });
+    new CfnOutput(this, "TheUrl", {
+      value: fnUrl.url,
+    });
   }
 }
