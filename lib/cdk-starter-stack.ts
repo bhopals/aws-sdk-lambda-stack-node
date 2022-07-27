4fa888ae9e6304c5b5b8187fef0ff9318a1010ef
Bhopal Singh <bhopalsinghsis@gmail.com>
2022-07-27 15:39:08 -0400

    Lambda Stack to spin up genenrator lambda with ROLE

--- lib/cdk-starter-stack.ts
+++ lib/cdk-starter-stack.ts
@@ -1,10 +1,17 @@
 import * as path from "path";
 import * as cdk from "aws-cdk-lib";
 import * as lambda from "aws-cdk-lib/aws-lambda";
+import { Bucket } from "aws-cdk-lib/aws-s3";
 
 import { CfnOutput, Aws } from "aws-cdk-lib";
 import { NodejsFunction } from "aws-cdk-lib/aws-lambda-nodejs";
-import { Role, ServicePrincipal, PolicyStatement } from "aws-cdk-lib/aws-iam";
+import {
+  Role,
+  ServicePrincipal,
+  PolicyStatement,
+  Effect,
+  AnyPrincipal,
+} from "aws-cdk-lib/aws-iam";
 
 import {
   LambdaType,
@@ -17,12 +24,34 @@ import {
   PRIVATE_LAMBDA_PATH,
   FUN_LABEL,
   ARN_LABEL,
+  S3_BUCKET_ID,
+  S3_PRINCIPAL,
+  S3_PUT_OBJECT,
+  S3_DELETE_OBJECT,
+  S3_GET_OBJECT,
+  GENERATOR_LAMBDA_URL,
+  GENERATOR_LAMBDA_PATH,
+  ROLE_ARN,
 } from "./stackConfiguration";
+
+import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
+
+const fs = require("fs");
+const admZip = require("adm-zip");
+
+const file = new admZip();
 
 export class CdkStarterStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
+    /*** CREATE S3 BUCKET */
+    const codeName = "test.zip";
+    this.createLambdaZIPFile();
+    const bucket = this.createS3Bucket();
+    this.uploadLambdaInBucket(bucket.bucketName, codeName);
+
+    /**** ROLE */
     const role = new Role(this, `${appName}-${LambdaRole.NAME}`, {
       assumedBy: new ServicePrincipal(LambdaRole.SERVICE_PRINCIPAL),
     });
@@ -33,7 +62,7 @@ export class CdkStarterStack extends cdk.Stack {
           `${ARN_LABEL}${Aws.REGION}:${Aws.ACCOUNT_ID}${FUN_LABEL}${appName}-${LambdaType.PUBLIC_LAMBDA}`,
           `${ARN_LABEL}${Aws.REGION}:${Aws.ACCOUNT_ID}${FUN_LABEL}${appName}-${LambdaType.PRIVATE_LAMBDA}`,
         ],
-        actions: [LambdaRole.ACTIONS],
+        actions: ["lambda:*"],
       })
     );
 
@@ -43,7 +72,7 @@ export class CdkStarterStack extends cdk.Stack {
       `${appName}-${LambdaType.PRIVATE_LAMBDA}`,
       {
         memorySize: 1024,
-        timeout: cdk.Duration.seconds(5),
+        timeout: cdk.Duration.seconds(500),
         runtime: lambda.Runtime.NODEJS_16_X,
         handler,
         role,
@@ -63,7 +92,7 @@ export class CdkStarterStack extends cdk.Stack {
       {
         memorySize: 1024,
         functionName: `${appName}-${LambdaType.PUBLIC_LAMBDA}`,
-        timeout: cdk.Duration.seconds(5),
+        timeout: cdk.Duration.seconds(500),
         runtime: lambda.Runtime.NODEJS_16_X,
         handler,
         role,
@@ -72,6 +101,37 @@ export class CdkStarterStack extends cdk.Stack {
           minify: false,
           externalModules: [AWS_SDK],
         },
+        environment: {
+          bucketName: bucket.bucketName,
+          bucketArn: bucket.bucketArn,
+          roleArn: role.roleArn,
+          bucketKey: codeName,
+        },
+      }
+    );
+
+    /*** FUNCTION CREATOR LAMBDA */
+    const fnCreatorLambda = new NodejsFunction(
+      this,
+      `${appName}-${LambdaType.GENERATE_LAMBDA}`,
+      {
+        memorySize: 1024,
+        functionName: `${appName}-${LambdaType.GENERATE_LAMBDA}`,
+        timeout: cdk.Duration.seconds(500),
+        runtime: lambda.Runtime.NODEJS_16_X,
+        handler,
+        role,
+        entry: path.join(__dirname, GENERATOR_LAMBDA_PATH),
+        bundling: {
+          minify: false,
+          externalModules: [AWS_SDK],
+        },
+        environment: {
+          bucketName: bucket.bucketName,
+          bucketArn: bucket.bucketArn,
+          roleArn: role.roleArn,
+          bucketKey: codeName,
+        },
       }
     );
 
@@ -79,8 +139,64 @@ export class CdkStarterStack extends cdk.Stack {
       authType: lambda.FunctionUrlAuthType.NONE,
     });
 
+    const fnCreatorUrl = fnCreatorLambda.addFunctionUrl({
+      authType: lambda.FunctionUrlAuthType.NONE,
+    });
+
     new CfnOutput(this, PUBLIC_LAMBDA_URL, {
       value: fnUrl.url,
     });
+
+    new CfnOutput(this, GENERATOR_LAMBDA_URL, {
+      value: fnCreatorUrl.url,
+    });
+
+    new CfnOutput(this, ROLE_ARN, {
+      value: role.roleArn,
+    });
+  }
+
+  private createS3Bucket() {
+    const bucket = new Bucket(this, S3_BUCKET_ID, {
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      autoDeleteObjects: true,
+    });
+    bucket.addToResourcePolicy(
+      new PolicyStatement({
+        effect: Effect.ALLOW,
+        principals: [new AnyPrincipal()],
+        actions: [S3_GET_OBJECT, S3_DELETE_OBJECT, S3_PUT_OBJECT],
+        resources: [`${bucket.bucketArn}/${S3_PRINCIPAL}`],
+      })
+    );
+    return bucket;
+  }
+
+  private async uploadLambdaInBucket(bucketName: string, codeName: string) {
+    const client = new S3Client({});
+    const putCommand = new PutObjectCommand({
+      Bucket: bucketName,
+      Key: codeName,
+      Body: fs.createReadStream(`./${codeName}`),
+    });
+
+    try {
+      const s3PutResponse = await client.send(putCommand);
+      console.log("S3 PUT Response: " + s3PutResponse);
+    } catch (err) {
+      console.log("ERROR in uploading the lambda");
+    }
+  }
+
+  private createLambdaZIPFile() {
+    file.addFile(
+      "test.js",
+      Buffer.from(`
+        exports.handler = async function(event, context) {
+          return { statusCode: 200, body: 'Hello, World!!!!' };
+        };
+      `)
+    );
+    file.writeZip("./test.zip");
   }
 }
--- lib/stackConfiguration.ts
+++ lib/stackConfiguration.ts
@@ -12,7 +12,10 @@ export const RDS_ARN = "AR:<>";
 
 export const BASE_PATH: string = "/../src/lambda";
 export const PUBLIC_LAMBDA_URL: string = "publicLambdaUrl";
+export const GENERATOR_LAMBDA_URL: string = "publicLambdaUrl";
+
 export const PUBLIC_LAMBDA_PATH: string = `${BASE_PATH}/index.ts`;
+export const GENERATOR_LAMBDA_PATH: string = `${BASE_PATH}/generateFn.ts`;
 export const PRIVATE_LAMBDA_PATH: string = `${BASE_PATH}/internal.ts`;
 
 export enum LambdaRole {
@@ -22,6 +25,18 @@ export enum LambdaRole {
 }
 
 export enum LambdaType {
+  GENERATE_LAMBDA = "generate-lambda",
   PUBLIC_LAMBDA = "public-lambda",
   PRIVATE_LAMBDA = "private-lambda",
 }
+
+export const S3_BUCKET_NAME = "s3-bucket";
+export const S3_BUCKET_ARN = "s3-bucket-arn";
+export const S3_BUCKET_ID = `${appName}-app-details-bucket`;
+
+export const S3_PRINCIPAL = "*";
+export const S3_GET_OBJECT = "s3:GetObject";
+export const S3_PUT_OBJECT = "s3:PutObject";
+export const S3_DELETE_OBJECT = "s3:DeleteObject";
+
+export const ROLE_ARN = "ROLE-ARN";
